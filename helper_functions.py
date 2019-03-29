7dbe4634cfbfebf43549c7017ff58458989a20cf
Reynaldo Martinez <rmartinez76@miners.utep.edu>
2019-03-29 11:24:15 -0600

    added mlp in tensorflow draft

---
+++ helper_functions.py
@@ -0,0 +1,117 @@
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_mnist(flatten=False, one_hot=False):
+    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+    # normalize x
+    X_train = X_train.astype(float) / 255.
+    X_test = X_test.astype(float) / 255.
+    # we reserve the last 10000 training examples for validation
+    X_train, X_val = X_train[:-10000], X_train[-10000:]
+    y_train, y_val = y_train[:-10000], y_train[-10000:]
+    if flatten:
+        X_train = X_train.reshape([X_train.shape[0], -1])
+        X_val = X_val.reshape([X_val.shape[0], -1])
+        X_test = X_test.reshape([X_test.shape[0], -1])
+    if one_hot:
+        y_train = one_hot_encode(y_train)
+        y_val = one_hot_encode(y_val)
+        y_test = one_hot_encode(y_test)
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def one_hot_encode(labels):
+    oh = np.zeros((len(labels), 10))
+    oh[np.arange(len(labels)), labels] = 1
+    return oh
+
+###############################
+#      PLOTTING RESULTS       #
+###############################
+
+'''
+
+'''
+def plot_model_results(train_log, test_log, train_loss, test_loss, params=False):
+    import seaborn as sns   # make it pretty
+    with sns.axes_style("darkgrid"):
+        
+        fig, axes = plt.subplots(nrows=1, ncols=2)
+
+        
+
+        # accuracy metrics
+        axes[0].plot(train_log, label='train accuracy')
+        axes[0].plot(test_log, label='test accuracy')
+        axes[0].set_title("Train Accuracy: %.3f"%train_log[-1])
+        axes[0].set_ylabel("Accuracy")
+        axes[0].set_xlabel("Epoch")
+        axes[0].legend(loc='best')
+
+        # loss metrics
+        axes[1].plot(train_loss, label='train loss')
+        axes[1].plot(test_loss, label='test loss')
+        axes[1].set_title("Train Loss: %.5f"%train_loss[-1])
+        axes[1].set_ylabel("Loss")
+        axes[1].set_xlabel("Epoch")
+        axes[1].legend(loc='best')
+        plt.show()
+
+        if params:
+            # title of figure
+            title = \
+            "Arch :: Epochs: %s | Batch Size: %s | Learning Rate: %s"\
+            %(params)
+            fig.suptitle(title)
+            # save figure
+            fig.savefig("mlp_e%s_b%s_lr%s.png"%(params))
+
+'''
+    function taken from: 
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+'''
+from sklearn.metrics import confusion_matrix
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
---
+++ mlp_example_tf.py
@@ -0,0 +1,146 @@
+import keras
+import numpy as np
+
+from helper_functions import * # plot_confusion_matrix, plot_model_results, load_mnist
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.metrics import accuracy_score
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_mnist(flatten=True, one_hot=True)
+
+# set random seed for replication
+tf.set_random_seed(42)
+
+# defining dimensions of network
+n_inputs = 784
+n_hidden_1 = 256
+n_hidden_2 = 256
+n_classes = 10
+
+# defining hyperparameters
+learning_rate = 0.001      # how fast it descends the gradient
+keep_prob = 0.6            # num of neuros that will activate
+epochs = 15                # num of forward & backward passes through whole training set
+batchsize = 128            # mini partitions of training set we train at a time
+display_results = True
+
+### Tensor Variables ###
+
+# tensor placeholders for inputs and outputs
+X = tf.placeholder("float", [None, n_inputs])  # input dimensions 
+Y = tf.placeholder("float", [None, n_classes]) # output dimensions
+dropout = tf.placeholder(tf.float32) # dropout regularization (input)
+
+# tensor variables for weights and biases 
+# weights are standardized to have mean = 0 and variance = 1
+weights = {
+    "w0" : tf.Variable(tf.random_normal([n_inputs, n_hidden_1], stddev=(1/tf.sqrt(float(n_inputs))))),
+    "w1" : tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], stddev=(1/tf.sqrt(float(n_hidden_1))))),
+    "w2" : tf.Variable(tf.random_normal([n_hidden_2, n_classes], stddev=(1/tf.sqrt(float(n_hidden_2)))))
+}
+
+biases = {
+    "b0" : tf.Variable(tf.random_normal([n_hidden_1])),
+    "b1" : tf.Variable(tf.random_normal([n_hidden_2])),
+    "b2" : tf.Variable(tf.random_normal([n_classes]))
+}
+
+### MODEL CREATION ###
+
+# define feedforward operation
+def forwardpass(X):
+    h1 = tf.add(tf.matmul(X, weights["w0"]), biases["b0"]) # ((W*X) + b)
+    h1_act = tf.nn.dropout(tf.nn.relu(h1), keep_prob)     # activations
+    
+    h2 = tf.add(tf.matmul(h1_act, weights["w1"]), biases["b1"])
+    h2_act = tf.nn.dropout(tf.nn.relu(h2), keep_prob)     # activations
+    
+    output = tf.add(tf.matmul(h2_act, weights["w2"]), biases["b2"])
+    output_act = tf.sigmoid(output)  # activations 
+    return output_act
+
+# graph operations
+logits = forwardpass(X)
+# loss_op = tf.reduce_mean(tf.losses.mean_squared_error(predictions=fpass, labels=Y))
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+train_op = optimizer.minimize(loss_op)
+
+### METRICS LOGS ###
+train_acc = []
+train_loss = []
+test_acc = []
+test_loss = []
+
+# def accuracy(actual, pred):
+#     correct_prediction = tf.equal(tf.argmax(actual, 1), tf.argmax(pred, 1))
+#     acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#     return acc
+
+correct_prediction = tf.equal(tf.argmax(y_train, 1), tf.argmax(logits, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) # cast a tensor to type float32
+
+### TRAINING MODEL ###
+# initializer operator
+init = tf.global_variables_initializer()
+
+# initialize a Session to run the Computation Graph
+with tf.Session() as sess:
+    # initialize model's global variables
+    sess.run(init)
+    
+    for e in range(epochs):
+        arr = np.arange(X_train.shape[0])
+        np.random.shuffle(arr) # shuffled indices
+        for i in range(0, X_train.shape[0], batchsize):
+            sess.run(train_op, {X: X_train[arr[i:i+batchsize]],
+                             Y: y_train[arr[i:i+batchsize]],
+                             dropout:keep_prob})
+    
+    	### RECORD METRICS AT EACH EPOCH ###
+    	# NOTE: The following is bad, TF has log functions for this
+    	# 		that we will get to see later in the workshop
+        
+        # training metrics 
+        train_acc.append(sess.run(accuracy, 
+        				feed_dict={X:X_train,
+        							Y: y_train,
+        							dropout: 1}))
+
+        train_loss.append(sess.run(loss_op, 
+        				feed_dict={X: X_train, 
+        							Y: y_train,
+        							dropout:1}))
+
+        # testing metrics 
+        test_acc.append(accuracy_score(y_test.argmax(1), 
+						sess.run(logits, feed_dict={
+							X:X_test, dropout: 1}).argmax(1)))
+        
+        test_loss.append(sess.run(loss_op, 
+        				feed_dict={X: X_test, 
+        						   Y: y_test,
+        						   dropout:1}))
+        
+        print("Epoch:{0}, Train loss: {1:.5f} Train acc: {2:.3f}, Test loss: {3:.5f} Test acc:{4:.3f}".format(e+1,
+                                                                    train_loss[-1],train_acc[-1],test_loss[-1],test_acc[-1]))
+
+    # predictions for confusion matrix
+    pred = sess.run(logits, feed_dict={X:X_test, dropout: 1}).argmax(1) #  predictions for confusion matrix
+
+
+### DISPLAY RESULTS ###
+
+if display_results:
+	# # plotting accuracy and loss graphs #
+	params = epochs, batchsize, str(learning_rate).replace('.', ',')
+	plot_model_results(train_acc, test_acc, 
+	                   train_loss, test_loss, params=params)
+
+
+	# confusion matrix #
+	class_names = [0,1,2,3,4,5,6,7,8,9]
+	# pred_test = predict(network, X_test)   # using test set (finally!)
+	y = np.argmax(y_test, axis=-1)           # needs to pass single value label not OH-encoding
+	plot_confusion_matrix(y, pred, classes=class_names, title="Confusion Matrix")
+	plt.show()
